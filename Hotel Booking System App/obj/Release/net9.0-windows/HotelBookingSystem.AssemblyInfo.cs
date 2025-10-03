5a68492d766bd019bc8f52803ff190da4f8ee26e
Domenic Carzo <domenic.carzo@gmail.com>
2025-10-03 17:56:59 +1000

    Additional setup stuff

--- Hotel Booking System App/obj/Release/net9.0-windows/HotelBookingSystem.AssemblyInfo.cs
+++ Hotel Booking System App/obj/Release/net9.0-windows/HotelBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a654c19ba77febf3d7199c4c14d616b17889d16")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
